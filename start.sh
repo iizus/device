13cc25e55520e0ebe6ae503eb0519df912482cac
iot-device-defender-user-at-317289014980 <iizus@amazon.co.jp>
2021-02-06 12:30:39 +0900

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
-#/bin/zsh
+#/usr/local/bin/fish
 
 DOCKER_MACHINE_ENV_NAME=$1
 
 open -a XQuartz
 docker-machine start $DOCKER_MACHINE_ENV_NAME
 eval $(docker-machine env $DOCKER_MACHINE_ENV_NAME)
+
+docker run --rm -it -e DISPLAY=$IP:0 gns3/xeyes
